cc2a1990b6fb9d72685b834f276ed66e40d493cf
dueonkim89 <dueonkim89@yahoo.com>
2020-11-27 21:32:25 -0800

    verificaton algo in poly time

--- js/script.js
+++ js/script.js
@@ -1,6 +1,13 @@
 // DOM elements for reference
 let numbersGrid = document.querySelectorAll(".col");
 let inputs = document.querySelectorAll("input");
+let newPuzzleButton = document.querySelector('.new-puzzle');
+let verifyButton = document.querySelector('.verify');
+
+let firstRow = document.querySelectorAll(".first-row")[0].children;
+let secondRow = document.querySelectorAll(".second-row")[0].children;
+let thirdRow = document.querySelectorAll(".third-row")[0].children;
+
 
 // 5 different puzzles to choose.
 let puzzles = {
@@ -93,15 +100,77 @@ let puzzles = {
   }
 };
 
-let start = 1;
+// puzzle starting number
+var start = 1;
+
+// function to change puzzle 
+function changePuzzle(numb) {
+  // start back at 1, if at 5
+  if (numb > 5) {
+    start = 1;
+    loadPuzzle(start);
+    return;
+  } 
+  loadPuzzle(numb);
+}
+
+// calback to verify user answer 
+function verifySolution() {
+  // create 9x9 grid for polynomial time verification.
+  let grid = createGrid(firstRow).concat(createGrid(secondRow)).concat(createGrid(thirdRow));
+
+  // loop thru the row and column of grid
+  for (let i = 0; i<grid.length; i++) {
+    // track values at each coordinate
+    let rowHash = {};
+    let colHash = {};
+    let squareHash = {};
+    for (let j = 0; j<grid[i].length; j++) {
+      // check each row and column
+      if (grid[i][j] === "" || rowHash[grid[i][j]] || colHash[grid[j][i]]) {
+        return false;
+      } 
+      // check each 3x3 square
+      let row = Math.floor(j / 3) + (Math.floor(i / 3) * 3);
+      let col = ((i % 3) * 3) + (j % 3);
+
+      if (squareHash[grid[row][col]]) {
+        return false
+      }
+      // else store all values in hash table
+      rowHash[grid[i][j]] = true;
+      colHash[grid[j][i]] = true;
+      squareHash[grid[row][col]] = true;
+
+      // console.log(i + 1 + 'cube', row, col);
+    }
+  }
+  return true;
+}
+
+function createGrid(row) {
+  // return 9 x 3 for each row
+  let grid = [[], [], []];
+
+  // loop thru first row
+  for (let i = 0; i<row.length; i++) {
+    // loop thru children of first row and construct grid
+    for (let j = 0; j<row[i].children.length; j++) {
+      if (j < 3) {
+        grid[0].push(row[i].children[j].children[0].value)
+      } else if (j > 2 && j < 6) {
+        grid[1].push(row[i].children[j].children[0].value)
+      } else {
+        grid[2].push(row[i].children[j].children[0].value)
+      }
+    }
+  }
+
+  return grid;
+}
 
 // load puzzle function
 function loadPuzzle(numb) {
-  // get the 3 rows of sudoku
-  let firstRow = document.querySelectorAll(".first-row")[0].children;
-  let secondRow = document.querySelectorAll(".second-row")[0].children;
-  let thirdRow = document.querySelectorAll(".third-row")[0].children;
-
   // load the data into the sudoku grid
   for (let row in puzzles[numb]) {
     // for each corresponding row, upload data into row
@@ -178,6 +247,18 @@ for (number of numbersGrid) {
 for (input of inputs) {
   input.addEventListener("click", event => textFocus(event.target));
 }
+
+// callback when pushing new puzzle button
+newPuzzleButton.addEventListener('click', () => {
+  start ++;
+  changePuzzle(start);
+});
+
+// callback when verifying user input
+verifyButton.addEventListener('click', () => {
+  console.log(verifySolution());
+  // give alert message if solved. if failed as well.
+});
 
 // load the puzzle
 loadPuzzle(start);
